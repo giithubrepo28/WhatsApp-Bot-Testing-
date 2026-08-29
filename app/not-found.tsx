cbbc55fca8084d618197c9558d2ab02682094870
giithubrepo28 <githubrepo28@gmail.com>
2026-08-30 04:42:10 +0500

    Add polished 404 experience

---
+++ app/not-found.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link';
+export default function NotFound(){return <main className="authPage"><div className="authCard" style={{textAlign:'center'}}><small className="label">THE ARCHIVES</small><h1>Lost between <i>worlds.</i></h1><p>The page you were looking for has drifted beyond the moonlight.</p><Link className="primary" href="/">Return to Elaria ↗</Link></div></main>}
